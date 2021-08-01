a0585e75d1577536547dc2297aba931620355461
sam7468 <samsharan7468@gmail.com>
2021-08-01 10:47:52 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ArticleOne from './ArticleOne'
+import ArticleTwo from './ArticleTwo'
+import ArticleThree from './ArticleThree'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './mystyles.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="all">
+      <ArticleOne/>
+      <ArticleTwo/>
+      <ArticleThree/>
     </div>
   );
 }
